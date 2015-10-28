a95997cb440dc3a9d68304d07f7996d2f96c85de
shaowns <shaown@csebuet.org>
2015-10-28 14:40:48 -0400

    Add solution for problem 6

---
+++ prob6.py
@@ -0,0 +1,11 @@
+def get_sum_difference(n):
+    return (n * (3 * n + 2) * (n ** 2 - 1)) / 12
+
+
+def main():
+    print get_sum_difference(100)
+    return 0
+
+
+if __name__ == '__main__':
+    main()
